128c0c2b8ae46e6faee31bd721cda9947fca392e
Deep Prajapati <dvprajapati7212@gmail.com>
2025-10-12 20:40:54 +0530

    feat: add TaskList and TaskListInfo components to EmployeeDashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../others/Header';
+import TaskListInfo from '../../others/TaskListInfo';
+import TaskList from '../TaskList/TaskList';
 
 const EmployeeDashboard = () => {
   return (
       <div className='p-10 bg-[#1C1C1C] h-screen'>
-          <Header/>
+      <Header />
+      <TaskListInfo />
+      <TaskList/>
     </div>
   )
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+      <div className='h-[40%] w-full bg-purple-400'>
+          TaskList
+      </div>
+  )
+}
+
+export default TaskList
