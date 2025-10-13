f811babfd550e090f41d29ad25980cba0e1d646e
Deep Prajapati <dvprajapati7212@gmail.com>
2025-10-13 21:45:04 +0530

    feat: update TaskList component layout and styling

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-      <div className='h-[40%] w-full bg-purple-400'>
-          TaskList
+      <div className='h-[55%] mt-10 py-5 px-5 w-full  flex items-center justify-start gap-5 flex-nowrap overflow-x-auto'>
+          <div className='flex-shrink-0 h-full rounded-xl w-[300px] bg-amber-300'></div>
+          <div className='flex-shrink-0 h-full rounded-xl w-[300px] bg-amber-300'></div>
+          <div className='flex-shrink-0 h-full rounded-xl w-[300px] bg-amber-300'></div>
+          <div className='flex-shrink-0 h-full rounded-xl w-[300px] bg-amber-300'></div>
+          <div className='flex-shrink-0 h-full rounded-xl w-[300px] bg-amber-300'></div>
       </div>
   )
 }
