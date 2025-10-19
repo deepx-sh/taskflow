f26fdad737b15b04616e1090fc3d31260fed2b76
Deep Prajapati <dvprajapati7212@gmail.com>
2025-10-19 22:45:07 +0530

    feat: add AllTask component and integrate it into AdminDashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../../others/Header";
 import CreateTask from "../../others/CreateTask";
+import AllTask from "../../others/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="p-10 bg-[#1C1C1C] h-screen  w-full">
       <Header />
       <CreateTask/>
-     
+     <AllTask/>
     </div>
   );
 };
