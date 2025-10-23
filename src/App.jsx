2249bffbf7dc48e755fc17e074f1858a98aefef1
Deep Prajapati <dvprajapati7212@gmail.com>
2025-10-23 21:13:23 +0530

    feat: implement authentication context and login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import React, { useEffect } from 'react'
-import Login from './components/Auth/Login';
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
-import AdminDashboard from './components/Dashboard/AdminDashboard';
-import { getLocalStorage, setLocalStorage } from './utils/localStorage';
+import React, { useContext, useEffect, useState } from "react";
+import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
+import { AuthContext } from "./context/AuthProviderContext";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+  console.log(authData.employees);
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  })
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      console.log("This is Admin");
+      setUser('admin');
+    } else if (authData && authData.employees.find((e)=> email==e.email && password==e.password)) {
+      console.log("This is User");
+      setUser('employee')
+    } else {
+      alert("Invalid Credentials");
+      
+    }
+  }
+
   
   return (
     <>
-      <Login/>
-      {/* <EmployeeDashboard/> */}
-        {/* <AdminDashboard/> */}
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user=='admin' ? <AdminDashboard/> : <EmployeeDashboard/>}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
