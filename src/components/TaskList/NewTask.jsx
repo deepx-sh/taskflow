6af34fff11cf43997c37052cc4929b0669b6759a
Deep Prajapati <dvprajapati7212@gmail.com>
2025-11-05 21:15:56 +0530

    style: add style accept button of new task component

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewTask = ({data}) => {
   return (
-    <div className="flex-shrink-0 w-[280px]  rounded-2xl sm:w-[300px] bg-gray-700 p-5 shadow-lg hover:scale-[1.03] transition-transform duration-300">
+    <div className="flex-shrink-0 w-[280px]  rounded-2xl sm:w-[300px] bg-blue-900/30 p-5 shadow-lg hover:scale-[1.03] transition-transform duration-300">
         <div className="flex justify-between items-center">
         <h3 className="bg-red-500/90 text-white px-3 py-1 rounded-full text-xs sm:text-sm font-medium uppercase tracking-wide">{ data.category}</h3>
         <h4 className="text-[10px] sm:text-xs text-gray-200">{ data.date}</h4>
@@ -12,7 +12,7 @@ const NewTask = ({data}) => {
       <p className="mt-3 text-xs sm:text-sm text-gray-100/90 leading-relaxed">{ data.description}</p>
           
           <div className='mt-4'>
-            <button>Accept Task</button>
+            <button className='w-full bg-blue-500 hover:bg-blue-600 py-2 px-4  font-medium transition-colors duration-200'>Accept Task</button>
           </div>
       </div>
   )
